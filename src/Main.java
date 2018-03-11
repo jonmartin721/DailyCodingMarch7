228be4dd643ed1374c26bd6f53928a34691f6a84
Jon Martin <jonmartin721@gmail.com>
2018-03-10 23:53:59 -0600

    Finished!

--- src/Main.java
+++ src/Main.java
@@ -16,17 +16,64 @@ public class Main {
         ArrayList<Integer> list = new ArrayList<>();
         Random rand = new Random();
 
-        for (int i = 0; i < 10; i++) {
-            list.add(i, rand.nextInt(((5-1)+ 1 ) + 1));
+        for (int i = 0; i < 4; i++) {
+            list.add(i, rand.nextInt(((5 - 1) + 1) + 1));
         }
 
-        multiplyArray(list);
+
+        ArrayList<Integer> newList = multiplyArray(list);
+
+        System.out.println("Old list: ");
+        for (Integer aList : list) {
+            System.out.print(aList + " ");
+        }
+        System.out.println("\n\nNew list: ");
+        for (Integer aNewList : newList) {
+            System.out.print(aNewList + " ");
+        }
 
     }
 
-    public static void multiplyArray(ArrayList<Integer> list) {
+    private static ArrayList<Integer> multiplyArray(ArrayList<Integer> list) {
 
+        //create a new list
         ArrayList<Integer> newList = new ArrayList<>();
+
+
+        //for each element in the original list
+        for (int i = 0; i < list.size(); i++) {
+
+            int multipliedValue = 0;
+            boolean notInitialized = true;
+
+            //look at all the other elements of the original list (other than the current one)
+            for (int j = 0; j < list.size(); j++) {
+
+
+                if (j != i) {
+
+                    //if this is the first element after multipliedValue was created
+                    if (notInitialized) {
+
+                        multipliedValue = list.get(j);
+                        notInitialized = false;
+                        continue; //and go to the next loop
+
+                    }
+
+                    multipliedValue *= list.get(j);
+
+                }
+
+
+            }
+
+            //after all other positions have been multiplied into multipliedValue
+            newList.add(i, multipliedValue);
+
+        }
+
+        return newList;
 
     }
 
